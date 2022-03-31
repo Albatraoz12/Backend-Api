60d96f1065a1a390991b1058e5a8e3c4e33fda2f
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-03-31 08:54:54 +0200

    add hasmany and belongsto at User and userList models

--- app/Models/userList.php
+++ app/Models/userList.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class userList extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'list_name',
+        'recipe_name'
+    ];
+    public function user()
+    {
+        $this->belongto(User::class);
+    }
 }

e0994de81f19f7de0460379aa0f3f4b688b68e5d
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-04-01 14:39:43 +0200

    add method getRecipe in recipeController

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\UserList;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    //
+    public function getRecipe(Request $request, $id)
+    {
+
+        $list = UserList::find($id);
+
+
+        $fields = $request->validate([
+            'list_name' => 'required|string'
+        ]);
+
+        $recipe = userList::create([
+            'title' => $fields['title'],
+            'image' => $fields['image'],
+            'recipe' => $fields['recipe'],
+            'user_list_id' => $list->id,
+        ]);
+
+        $response = [
+            'status' => true,
+            'message' => 'Recipe is successfully added to list!',
+        ];
+        return response($response, 201);
+    }
 }

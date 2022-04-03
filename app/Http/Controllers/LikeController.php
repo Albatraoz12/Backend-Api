33c0494a85a101eea13ab20b1eebae0a9cab2742
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-04-03 11:08:22 +0200

    fix getAllLikes function

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function getAllLikes(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        $fields = $request->validate([
+            'title' => 'required|string',
+            'image' => 'required|string',
+            'recipe' => 'required|integer'
+        ]);
+
+        $recipe = Like::create([
+            'title' => $fields['title'],
+            'image' => $fields['image'],
+            'recipe' => $fields['recipe'],
+            'user_id' => $user->id,
+        ]);
+
+        $response = [
+            'status' => true,
+            'message' => 'Recipe is successfully added to list!',
+        ];
+        return response($response, 201);
+    }
 }
